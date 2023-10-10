4df8af7beb09c85ec332067d757c304b0ceef3a6
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-10-10 15:21:32 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,7 +1,8 @@
 ui <- fluidPage(
   sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
   sliderInput("y", label = "If y is", min = 1, max = 50, value = 30),
-  "then x times y is",
-  textOutput("product")
+  "then (x*y) is",  textOutput("product"),
+  "then (x*y) +5 is",  textOutput("product_plus5"),
+  "then (x*y) +10 is",  textOutput("product_plus10"),
 )
   
