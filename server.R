e08d803b1b1fbb979528d834130b17920ad96cb9
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-10-10 15:20:19 -0400

    Update server.R
    adding +5 and +10 options to calculations with a reactive expression to reduce code duplication

--- server.R
+++ server.R
@@ -1,5 +1,18 @@
 server <- function(input, output, session) {
+
+  product <- reactive({
+    product <- input$x * input$y
+  })
+  
   output$product <- renderText({
-    input$x * input$y
+    product()
+  })
+
+  output$product_plus5 <- renderText({
+    product() + 5
+  })
+
+  output$product_plus10 <- renderText({
+    product() +10
   })
 }
